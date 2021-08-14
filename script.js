f6ba9ddfbaba57f08abb32e0e514a4ab5a899372
Odiri Teddie <odiriteddie.adamu@gmail.com>
2021-08-14 01:24:24 -0400

    modal-menu-slider

---
+++ script.js
@@ -0,0 +1,31 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+//  Toggle nav
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav');
+});
+
+// Show Modal
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal');
+});
+
+// Hide modal
+
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal');
+});
+
+// Hide modal 
+window.addEventListener('click', e => {
+    if(e.target == modal) {
+        modal.classList.remove('show-modal')
+    } else {
+        false;
+    }
+});
